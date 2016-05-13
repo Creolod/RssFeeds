ef3619e7885ecff517c5d111210223ab1663f97d
Станислав <stanislav@trainingvdi.abtis.de>
2016-05-13 12:04:47 +0600

    Initial Commit

---
+++ testRss/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testRss
+//
+//  Created by Станислав on 13.05.16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
